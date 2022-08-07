c09d66c149194ee7c33858f48b1d5fd35cc45501
zerop <zerop.development@gmail.com>
2022-08-07 23:05:54 +0300

    piont

--- public/main.js
+++ public/main.js
@@ -1,46 +1,59 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const readArguments = require('@zeropxwz/read-arguments');
 const hex_1 = require("./helpers/hex");
-function toDecim(num) {
-    var sys = num.match(/(^0b|0x)/g).join();
-    var num = num.replace(/(^0b|0x)/g, '');
-    switch (sys) {
-        case '0b':
-            sys = 2;
-            break;
-        case '0x':
-            sys = 16;
-            break;
+const args = readArguments(process.argv);
+let sysA = args[0].match(/^(0x|0b|0d)/g).join();
+let sysB = args[1];
+let num = args[0].replace(/^(0x|0b|0d)/, '');
+class Converter {
+    static toDecim(num, sys) {
+        switch (sys) {
+            case '0b':
+                sys = 2;
+                break;
+            case '0x':
+                sys = 16;
+                break;
+        }
+        var exp = num.length - 1;
+        var ind = 0;
+        var res = 0;
+        while (exp >= 0) {
+            sys === 2
+                ? res += Number(num[ind]) * Math.pow(Number(sys), exp)
+                : res += Number((0, hex_1.hexA)(num)[ind]) * Math.pow(Number(sys), exp);
+            exp--;
+            ind++;
+        }
+        console.log(res);
     }
-    var exp = num.length - 1;
-    var ind = 0;
-    var res = 0;
-    while (exp >= 0) {
-        sys === 2
-            ? res += Number(num[ind]) * Math.pow(Number(sys), exp)
-            : res += Number((0, hex_1.hexA)(num)[ind]) * Math.pow(Number(sys), exp);
-        exp--;
-        ind++;
+    static toOther(num, sys) {
+        var res = '';
+        switch (sys) {
+            case 'bin':
+                while (num !== 0) {
+                    res += String(num % 2) + '|';
+                    num = Math.floor(num / 2);
+                }
+                console.log(res.split('|').reverse().join().replace(/,/g, ''));
+                break;
+            case 'hex':
+                while (num !== 0) {
+                    res += String(num % 16) + '|';
+                    num = Math.floor(num / 16);
+                }
+                console.log((0, hex_1.hexB)(res.split('|')));
+                break;
+        }
     }
-    console.log(res);
 }
-function toOther(num, sys) {
-    var res = '';
-    switch (sys) {
-        case 'bin':
-            while (num !== 0) {
-                res += String(num % 2) + '|';
-                num = Math.floor(num / 2);
-            }
-            console.log(res.split('|').reverse().join().replace(/,/g, ''));
-            break;
-        case 'hex':
-            while (num !== 0) {
-                res += String(num % 16) + '|';
-                num = Math.floor(num / 16);
-            }
-            console.log((0, hex_1.hexB)(res.split('|')));
-            break;
+function convert(from, to, num) {
+    if (to === '0b' || to === '0x') {
+        Converter.toOther(Number(num), to);
+    }
+    else {
+        Converter.toDecim(num, from);
     }
 }
-toOther(Number('4'), 'bin');
+convert(sysA, sysB, num);
--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,103 @@
+const readArguments = require('@zeropxwz/read-arguments')
+
 import {hexA, hexB, hexTABLE} from "./helpers/hex"
 
-function toDecim (num: string): void {
 
-    var sys: string | number = num.match(/(^0b|0x)/g)!.join()
-    var num = num.replace(/(^0b|0x)/g, '')
 
-    switch (sys) {
-        case '0b':
-            sys = 2
-            break
-        case '0x':
-            sys = 16
-            break
+const args: Array<string> = readArguments(process.argv)
+
+
+let sysA: string = args[0].match(/^(0x|0b|0d)/g)!.join() 
+let sysB: string = args[1]
+let num:  string = args[0].replace(/^(0x|0b|0d)/, '')
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Converter {
+
+    static toDecim (num: string, sys: string | number): void {
+        switch (sys) {
+            case '0b':
+                sys = 2
+                break
+            case '0x':
+                sys = 16
+                break
+        }
+
+        var exp: number = num.length - 1
+        var ind: number = 0
+
+        var res: number = 0
+
+        while (exp >= 0) {
+            
+            sys === 2 
+                ? res += Number(num[ind])       * Math.pow(Number(sys), exp)
+                : res += Number(hexA(num)[ind]) * Math.pow(Number(sys), exp)
+
+            exp--
+            ind++
+        }
+
+        console.log(res)
     }
-
-    var exp: number = num.length - 1
-    var ind: number = 0
-
-    var res: number = 0
-
-    while (exp >= 0) {
+    static toOther (num: number, sys: string) {
+        var res: string = ''
         
-        sys === 2 
-            ? res += Number(num[ind])      * Math.pow(Number(sys), exp)
-            : res += Number(hexA(num)[ind]) * Math.pow(Number(sys), exp)
-
-        exp--
-        ind++
+        switch (sys) {
+            case 'bin':
+                while (num !== 0) {
+                    res += String(num % 2) + '|'
+                    num  = Math.floor(num / 2)
+                }
+            
+                console.log(res.split('|').reverse().join().replace(/,/g, ''))
+                break
+            case 'hex':
+                while (num !== 0) {
+                    res += String(num % 16) + '|'
+                    num  = Math.floor(num / 16)
+                }
+    
+                console.log(hexB(res.split('|')))
+                break
+        }
     }
-
-    console.log(res)
 }
 
-function toOther (num: number, sys: string): void {
+function convert (from: string, to: string, num: string): void {
 
-    var res: string = ''
-        
-    switch (sys) {
-        case 'bin':
-            while (num !== 0) {
-                res += String(num % 2) + '|'
-                num  = Math.floor(num / 2)
-            }
-        
-            console.log(res.split('|').reverse().join().replace(/,/g, ''))
-            break
-        case 'hex':
-            while (num !== 0) {
-                res += String(num % 16) + '|'
-                num  = Math.floor(num / 16)
-            }
-
-            console.log(hexB(res.split('|')))
-            break
+    if (to === '0b' || to === '0x') {
+        Converter.toOther(Number(num), to)
+    }
+    else {
+        Converter.toDecim(num, from)
     }
 
-
 }
-toOther(Number('4'), 'bin')
+
+convert(sysA, sysB, num)
+
+
+
+
+
+
+
+
+
+
+
 
 
