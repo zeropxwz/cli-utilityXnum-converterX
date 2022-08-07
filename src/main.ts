469b1ad52037144cccb6f77e7c9c82e09c826b04
zerop <zerop.development@gmail.com>
2022-08-07 11:18:44 +0300

    small fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@
 
 const hexTABLE = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',]
 
-function hexTranslate (num: string): Array<string> {
-    var x: Array<string> = [] 
+function hex (num: string): Array<string> {
+    const x: Array<string> = [] 
 
     for (let i = 0; i < num.length; i++) {
         for (let j = 0; j < hexTABLE.length; j++) {
@@ -22,54 +22,38 @@ function hexTranslate (num: string): Array<string> {
 function toDecim (num: string): void {
 
     var sys: string | number = num.match(/(^0b|0x)/g)!.join()
-    var num 
-        = num.replace(/(^0b|0x)/g, '')
+    var num = num.replace(/(^0b|0x)/g, '')
 
     switch (sys) {
         case '0b':
             sys = 2
-
-            var exp: number = num.length - 1
-            var ind: number = 0
-        
-            var res: number = 0
-        
-            while (exp >= 0) {
-                res += Number(num[ind]) * Math.pow(Number(sys), exp)
-        
-                exp--
-                ind++
-            }
-        
-            console.log(res)
             break
         case '0x':
             sys = 16
-
-            const hexNUM = hexTranslate(num)
-
-            var exp: number = num.length - 1
-            var ind: number = 0
-        
-            var res: number = 0
-        
-            while (exp >= 0) {
-                res += Number(hexNUM[ind]) * Math.pow(Number(sys), exp)
-        
-                exp--
-                ind++
-            }
-        
-            console.log(res)
             break
     }
 
+    var exp: number = num.length - 1
+    var ind: number = 0
 
+    var res: number = 0
+
+    while (exp >= 0) {
+        
+        sys === 2 
+            ? res += Number(num[ind])      * Math.pow(Number(sys), exp)
+            : res += Number(hex(num)[ind]) * Math.pow(Number(sys), exp)
+
+        exp--
+        ind++
+    }
+
+    console.log(res)
+}
+toDecim('0b10111')
+
+function toOther (num: string): void {
 
 }
-
-toDecim('0xA9')
-
-
 
 
